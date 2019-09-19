6b7389a1b56a37d6eef065620a44d18d1754be94
Conrad Schloer <conrad.schloer@gmail.com>
2019-09-19 16:29:33 -0400

    Add support for convert_to_integer parameter

--- bcodmo_processors/bcodmo_pipeline_processors/round_fields.py
+++ bcodmo_processors/bcodmo_pipeline_processors/round_fields.py
@@ -8,9 +8,30 @@ parameters, datapackage, resource_iterator = ingest()
 resources = ResourceMatcher(parameters.get('resources'), datapackage)
 fields = parameters.get('fields', [])
 
+def modify_datapackage(datapackage_):
+    dp_resources = datapackage_.get('resources', [])
+    for resource_ in dp_resources:
+        if resources.match(resource_['name']):
+            datapackage_fields = resource_['schema']['fields']
+            for field in fields:
+                # If we should cast to an integer rather than a number
+                if field.get('convert_to_integer', False) and field['digits'] == 0:
+                    def convert_to_integer(f):
+                        if f['name'] == field['name']:
+                            if f['type'] is not 'number':
+                                raise Exception(
+                                    f'Attempting to convert a field ("{f["name"]}") that has not been cast to a number'
+                                )
+                            f['type'] = 'integer'
+                        return f
+
+                    for datapackage_field in datapackage_fields:
+                        datapackage_fields = list(map(convert_to_integer, datapackage_fields))
+
+            resource_['schema']['fields'] = datapackage_fields
+    return datapackage_
 
 def process_resource(rows, missing_data_values, schema):
-    logging.info(schema)
     dp_resources = datapackage.get('resources', [])
     row_counter = 0
     for row in rows:
@@ -22,9 +43,10 @@ def process_resource(rows, missing_data_values, schema):
                     if schema_field['name'] == field['name']:
                         cur_schema_field = schema_field
                         break
-                logging.info(cur_schema_field)
                 # Check if the type in the datapackage is a number
-                if cur_schema_field['type'] == 'number':
+                if cur_schema_field['type'] == 'number' or (
+                    cur_schema_field['type'] == 'integer' and field['digits'] == 0 and field.get('convert_to_integer', False)
+                ):
                     orig_val = row[field['name']]
                     if orig_val in missing_data_values or orig_val is None:
                         row[field['name']] = orig_val
@@ -34,6 +56,12 @@ def process_resource(rows, missing_data_values, schema):
                     # If the field has been cast to a number without validating,
                     # it might be a string here
                     new_val = type(orig_val)(str(rounded_val))
+                    #
+                    if field.get('convert_to_integer', False) and field['digits'] == 0:
+                        # If we have cast to integer AND validated, we can set the type here to integer
+                        if type(orig_val) is not 'string':
+                            new_val = int(new_val)
+
                     row[field['name']] = new_val
                 else:
                     raise Exception(
@@ -66,4 +94,4 @@ def process_resources(resource_iterator_):
             yield process_resource(resource, missing_data_values, schema)
 
 
-spew(datapackage, process_resources(resource_iterator))
+spew(modify_datapackage(datapackage), process_resources(resource_iterator))
