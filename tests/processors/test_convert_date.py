244862e9423ccdaa35ead88c20120b12bf768284
Conrad S <conrad.schloer@gmail.com>
2020-04-10 11:34:32 +0200

    Add 5 new tests

--- tests/processors/test_convert_date.py
+++ tests/processors/test_convert_date.py
@@ -10,15 +10,27 @@ from bcodmo_processors.bcodmo_pipeline_processors import *
 
 TEST_DEV = os.environ.get("TEST_DEV", False)
 
+# datetime
 data_1 = [
     {"col1": "12/31/1995 11:24:31"},
 ]
 
+# multiple
 data_2 = [
     {"col1": "12", "col2": "31", "col3": "1995"},
 ]
 
+# timezone
 data_3 = [{"col1": "12/31/1999 23:59:59"}]
+
+# decimalDay
+data_4 = [{"col1": "74.324"}]
+
+# excel
+data_5 = [{"col1": "43510.32"}]
+
+# test order
+data_6 = [{"fake1": "data", "col1": "12/31/1995", "fake2": "data"}]
 
 
 @pytest.mark.skipif(TEST_DEV, reason="test development")
@@ -173,3 +185,78 @@ def test_convert_date_year():
     ]
     rows, datapackage, _ = Flow(*flows).results()
     assert rows[0][0]["datetime_field"] == dateutil.parser.parse("12/31/1995")
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_convert_date_decimal_day():
+    flows = [
+        data_4,
+        convert_date(
+            {
+                "fields": [
+                    {
+                        "input_type": "decimalDay",
+                        "input_field": "col1",
+                        "output_field": "datetime_field",
+                        "output_format": "%Y-%m-%dT%H:%M:%SZ",
+                        "output_type": "datetime",
+                        "year": "1995",
+                    }
+                ]
+            }
+        ),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    assert rows[0][0]["datetime_field"] == dateutil.parser.parse("3/15/1995 7:46:33.6")
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_convert_date_excel():
+    flows = [
+        data_5,
+        convert_date(
+            {
+                "fields": [
+                    {
+                        "input_type": "excel",
+                        "input_field": "col1",
+                        "output_field": "datetime_field",
+                        "output_format": "%Y-%m-%dT%H:%M:%SZ",
+                        "output_type": "datetime",
+                    }
+                ]
+            }
+        ),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    assert rows[0][0]["datetime_field"] == dateutil.parser.parse("2/14/2019 7:40:48")
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_convert_date_order():
+    # Ensure that the order of fields remain the same when a field is overwritten
+    flows = [
+        data_6,
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    prev_field_names = [f.name for f in datapackage.resources[0].schema.fields]
+
+    flows = [
+        data_6,
+        convert_date(
+            {
+                "fields": [
+                    {
+                        "inputs": [{"field": "col1", "format": "%m/%d/%Y"}],
+                        "output_field": "col1",
+                        "output_format": "%Y-%m-%dT%H:%M:%SZ",
+                        "output_type": "datetime",
+                    }
+                ]
+            }
+        ),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    after_field_names = [f.name for f in datapackage.resources[0].schema.fields]
+
+    assert prev_field_names == after_field_names
---
+++ tests/processors/test_split_column.py
@@ -0,0 +1,58 @@
+import pytest
+import os
+from dataflows import Flow, set_type
+from decimal import Decimal
+
+from bcodmo_processors.bcodmo_pipeline_processors import *
+
+
+TEST_DEV = os.environ.get("TEST_DEV", False)
+
+data = [
+    {"col1": "hello world"},
+    {"col1": "foo bar"},
+]
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_split_column():
+    flows = [
+        data,
+        split_column(
+            {
+                "fields": [
+                    {
+                        "input_field": "col1",
+                        "pattern": "(.*) (.*)",
+                        "output_fields": ["f1", "f2"],
+                    }
+                ]
+            }
+        ),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    assert rows[0][0]["col1"] == "hello world"
+    assert rows[0][0]["f1"] == "hello"
+    assert rows[0][0]["f2"] == "world"
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_split_column_delete_input():
+    flows = [
+        data,
+        split_column(
+            {
+                "fields": [
+                    {
+                        "input_field": "col1",
+                        "pattern": "(.*) (.*)",
+                        "output_fields": ["f1", "f2"],
+                    }
+                ],
+                "delete_input": True,
+            }
+        ),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    assert "col1" not in rows[0][0]
+    assert "col1" not in [f.name for f in datapackage.resources[0].schema.fields]
