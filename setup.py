1badb0acd264708b6a1d75b490e640febfc5e9a8
Conrad S <conrad.schloer@gmail.com>
2020-04-14 10:52:00 +0200

    Removal of pandas

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
 
 required = [
-    'datapackage-pipelines==2.1.10',
-    'pyparsing==2.2.0',
-    'dataflows==0.0.65',
-    'tabulator==1.34.0',
-    'tableschema==1.15.0',
-    'pandas',
-
+    "datapackage-pipelines==2.1.10",
+    "pyparsing==2.2.0",
+    "dataflows==0.0.65",
+    "tabulator==1.34.0",
+    "tableschema==1.15.0",
 ]
 
 
 setup(
-    name='bcodmo_processors',
-    version='v0.0.1',
-    description='BCODMO Custom Processors',
-    author='BCODMO',
-    author_email='conrad.schloer@gmail.com',
-    url='https://github.com/bcodmo/bcodmo_processors',
+    name="bcodmo_processors",
+    version="v0.0.1",
+    description="BCODMO Custom Processors",
+    author="BCODMO",
+    author_email="conrad.schloer@gmail.com",
+    url="https://github.com/bcodmo/bcodmo_processors",
     packages=find_packages(),
     install_requires=required,
     include_package_data=True,
 )
-
