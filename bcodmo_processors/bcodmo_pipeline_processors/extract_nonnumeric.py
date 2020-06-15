0d946df50edc1b0b42ed9eda9739ef1ebcbb48f8
Conrad S <conrad.schloer@gmail.com>
2020-06-15 16:21:41 +0200

    Add extract_nonnumeric processor

---
+++ bcodmo_processors/bcodmo_pipeline_processors/extract_nonnumeric.py
@@ -0,0 +1,113 @@
+import sys
+from datetime import datetime, timedelta
+from dateutil.tz import tzoffset
+from decimal import Decimal, InvalidOperation
+import logging
+import pytz
+import re
+import math
+
+from dataflows import Flow
+from dataflows.helpers.resource_matcher import ResourceMatcher
+from datapackage_pipelines.wrapper import ingest
+from datapackage_pipelines.utilities.flow_utils import spew_flow
+
+from bcodmo_processors.bcodmo_pipeline_processors.boolean_processor_helper import (
+    get_expression,
+    check_line,
+)
+from bcodmo_processors.bcodmo_pipeline_processors.helper import get_missing_values
+
+
+def process_resource(rows, fields, missing_values, suffix=None, boolean_statement=None):
+    expression = get_expression(boolean_statement)
+
+    row_counter = 0
+    for row in rows:
+        row_counter += 1
+
+        line_passed = check_line(expression, row_counter, row, missing_values)
+        try:
+            for field in fields:
+                new_field_name = f"{field}{suffix}"
+                if not line_passed:
+                    if new_field_name not in row:
+                        row[new_field_name] = ""
+                    continue
+
+                # Check if  the field is numeric
+                original_value = row.get(field, None)
+                try:
+                    float(original_value)
+                    is_numeric = True
+                except ValueError:
+                    is_numeric = False
+
+                if is_numeric:
+                    row[new_field_name] = ""
+                else:
+                    row[new_field_name] = row[field]
+                    row[field] = None
+
+            yield row
+        except Exception as e:
+            raise type(e)(str(e) + f" at row {row_counter}").with_traceback(
+                sys.exc_info()[2]
+            )
+
+
+def extract_nonnumeric(fields, resources=None, suffix="_", boolean_statement=None):
+    def func(package):
+        matcher = ResourceMatcher(resources, package.pkg)
+        for resource in package.pkg.descriptor["resources"]:
+            if matcher.match(resource["name"]):
+                # Get the old fields
+                package_fields = resource["schema"]["fields"]
+                package_field_names = [f["name"] for f in package_fields]
+                new_fields = []
+                for field in package_fields:
+                    new_fields.append(field)
+                    if field.get("name", None) in fields:
+                        new_field_name = f'{field.get("name")}{suffix}'
+                        if new_field_name in package_field_names:
+                            raise Exception(
+                                f'The new field "{new_field_name}" already exists in the datapackage.'
+                            )
+                        new_fields.append(
+                            {"name": new_field_name, "type": "string",}
+                        )
+
+                # Add back to the datapackage
+                resource["schema"]["fields"] = new_fields
+        yield package.pkg
+
+        for rows in package:
+            if matcher.match(rows.res.name):
+                missing_values = get_missing_values(rows.res)
+                yield process_resource(
+                    rows,
+                    fields,
+                    missing_values,
+                    suffix=suffix,
+                    boolean_statement=boolean_statement,
+                )
+            else:
+                yield rows
+
+    return func
+
+
+def flow(parameters):
+    return Flow(
+        extract_nonnumeric(
+            parameters.get("fields", []),
+            resources=parameters.get("resources"),
+            suffix=parameters.get("suffix", "_"),
+            boolean_statement=parameters.get("boolean_statement"),
+        )
+    )
+
+
+if __name__ == "__main__":
+    with ingest() as ctx:
+        spew_flow(flow(ctx.parameters), ctx)
---
+++ tests/processors/test_extract_nonnumeric.py
@@ -0,0 +1,36 @@
+import pytest
+import os
+from dataflows import Flow, set_type
+from decimal import Decimal
+
+from bcodmo_processors.bcodmo_pipeline_processors import *
+
+
+TEST_DEV = os.environ.get("TEST_DEV", False)
+
+data = [
+    {"col1": "42"},
+    {"col1": "15.2"},
+    {"col1": "this is a string"},
+    {"col1": "341 and a string"},
+    {"col1": "12"},
+]
+
+
+@pytest.mark.skipif(TEST_DEV, reason="test development")
+def test_extract_nonnumeric():
+    flows = [
+        data,
+        extract_nonnumeric({"fields": ["col1"], "suffix": "_comment"}),
+    ]
+    rows, datapackage, _ = Flow(*flows).results()
+    assert rows[0][0]["col1"] == "42"
+    assert rows[0][0]["col1_comment"] == None
+    assert rows[0][2]["col1"] == None
+    assert rows[0][2]["col1_comment"] == "this is a string"
+    assert rows[0][3]["col1"] == None
+    assert rows[0][3]["col1_comment"] == "341 and a string"
+
+    fields = datapackage.resources[0].schema.fields
+    assert fields[0].name == "col1"
+    assert fields[1].name == "col1_comment"
