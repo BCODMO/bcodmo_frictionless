a5da5ec394d4fb258095381be94c107db996fb7e
Conrad S <conrad.schloer@gmail.com>
2020-06-16 13:20:34 +0200

    Bump dataflows version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ required = [
     "pandas==0.25.3",
     "datapackage-pipelines==2.1.10",
     "pyparsing==2.2.0",
-    "dataflows==0.1.0",
+    "dataflows==0.1.1",
     "tabulator==1.52.0",
     "tableschema==1.15.0",
 ]
@@ -12,7 +12,7 @@ required = [
 
 setup(
     name="bcodmo_processors",
-    version="v2.0.1",
+    version="v2.0.2",
     description="BCODMO Custom Processors",
     author="BCODMO",
     author_email="conrad.schloer@gmail.com",
