6ff6615b51cde062efa5e989c7520261b2c7bad8
Conrad S <conrad.schloer@gmail.com>
2020-06-26 12:33:24 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ required = [
 
 setup(
     name="bcodmo_processors",
-    version="v2.0.2",
+    version="v2.0.3",
     description="BCODMO Custom Processors",
     author="BCODMO",
     author_email="conrad.schloer@gmail.com",
