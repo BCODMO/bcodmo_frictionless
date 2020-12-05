4488d0a828fb7626eaa267489a28c4c3aaa1bed3
Conrad S <conrad.schloer@gmail.com>
2020-12-05 13:52:31 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ required = [
 
 setup(
     name="bcodmo_frictionless",
-    version="v2.0.6",
+    version="v2.0.7",
     description="BCODMO Custom Processors and Checks",
     author="BCODMO",
     author_email="conrad.schloer@gmail.com",
